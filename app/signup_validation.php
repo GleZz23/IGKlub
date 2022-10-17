46c0ffc97701783757d7c4e671f46b5d4bc9e52d
GleZz23 <ikerglez110@gmail.com>
2022-10-17 17:58:41 +0200

    Registro de alumnos

--- app/signup_validation.php
+++ app/signup_validation.php
@@ -1,30 +1,38 @@
 <?php
 include_once('connection.php');
 
+// Recoger en variables los datos introducidos en el formulario
 $nickname = $_GET["nickname"];
 $email = $_GET["email"];
+$name = $_GET["name"];
+$surnames = $_GET["surnames"];
+$date = $_GET["date"];
+$password = $_GET["password"];
+$password = password_hash($password, PASSWORD_DEFAULT);
 
-$query = $miPDO->prepare('SELECT COUNT(*) FROM usuario WHERE nickname=:nickname OR email=:email');
-$query->execute(['nickname' => $nickname,
-                  'email' => $email]);
+// Comprobar que el nickname no existe en la base de datos
+$query = $miPDO->prepare('SELECT COUNT(*) FROM usuario WHERE nickname=:nickname');
+$query->execute(['nickname' => $nickname]);
 $results = $query->fetchColumn();
 
 if ($results > 0) {
-  echo 'Nickname o Email en uso';
+  $nickname_error = 'Nickname en uso';
+  header('Location: ../signup.php');
+  // Mostrar error en el formulario
 } else {
-  $query = $miPDO->prepare('INSERT INTO usuario (nickname, email, nombre, apellidos, fecha_nacimiento, contrasena, rol)
-                          VALUES (:nickname, :email, :name, :surnames, :date, :password, "ikasle")');
+  // Comprobar que el email no existe en la base de datos
+  $query = $miPDO->prepare('SELECT COUNT(*) FROM usuario WHERE email=:email');
+  $query->execute(['email' => $email]);
+  $results = $query->fetchColumn();
 
-  $name = $_GET["name"];
-  $surnames = $_GET["surnames"];
-  $date = $_GET["date"];
-  $password = $_GET["password"];
-  $password = password_hash($password, PASSWORD_DEFAULT);
-
-  $query->execute(['nickname' => $nickname,
-                  'email' => $email,
-                  'name' => $name,
-                  'surnames' => $surnames,
-                  'date' => $date,
-                  'password' => $password]);
+  if ($results > 0) {
+    echo 'Email en uso';
+    header('Location: ../signup.php');
+    // Mostrar error en el formulario
+  } else {
+    // Inserto el usuario en la base de datos
+    $query = $miPDO->prepare('INSERT INTO usuario (nickname, email, nombre, apellidos, fecha_nacimiento, contrasena, rol) VALUES (:nickname, :email, :name, :surnames, :date, :password, "ikasle")');
+    $query->execute(['nickname' => $nickname, 'email' => $email, 'name' => $name, 'surnames' => $surnames, 'date' => $date, 'password' => $password]);
+    header('Location: ../login.php');
+  }
 }
