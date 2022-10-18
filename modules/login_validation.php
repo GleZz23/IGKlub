5f4f9fa6e17a53ed92374a44c16c41f686aa31b9
GleZz23 <ikerglez110@gmail.com>
2022-10-18 16:49:11 +0200

    -

--- modules/login_validation.php
+++ modules/login_validation.php
@@ -1,24 +1,17 @@
 <?php
-// ABRIR LA CONEXION
-include("connection.php");
-session_start();
-$nickname = $_SESSION['nickname'];
-$password = $_SESSION['password'];
+include_once('connection.php');
 $nickname = $_GET['nickname'];
 $password = $_GET['password'];
 
+$query = $miPDO->prepare('SELECT nickname, contrasena FROM usuario WHERE nickname =:nickname;');
+$query->execute(['nickname' => $nickname]);
+$result = $query->fetch();
 
-// Prepara consulta para comprobar que el usuario y la contraseña coinciden
-$miConsulta = $miPDO->prepare('SELECT count(*) FROM usuario WHERE nickname =:nickname AND contrasena =:pass;');
-// Ejecuta consulta
-$miConsulta->execute(['nickname' => $nickname,'pass' => $password]);
-$resultado= $miConsulta->fetchColumn();
-// Si el usuario exite y la contraseña coincide el resultado
-// de la consulta dara mas que 0
-if ($resultado >0) {
-    header('Location: ../index.php'); //Cambiar por el menu principal
-    // Si el usuario no existe o no coinciden
-} else{
-    header('Location: ../views/loginToRegister.php');
-}
+    if (password_verify($password, $result['contrasena'])) {
+        session_start();
+        $_SESSION['nickname'] = $result['nickname'];
+        header('Location: ../views/account_status.php');
+    } else{
+        header('Location: ../views/loginToRegister.php');
+    }
 ?>
