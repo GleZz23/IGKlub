fd3f119da8c7a74435a1ae6b7cd2f6906b278f3a
GleZz23 <ikerglez110@gmail.com>
2022-10-19 18:04:07 +0200

    -

--- js/signup_validation.js
+++ js/signup_validation.js
@@ -1,109 +1,160 @@
-const formulario = document.getElementById('formularioRegistrar');
-const inputs = document.querySelectorAll('#formularioRegistrar input');
+const form = document.getElementById('singupForm');
+const inputs = document.querySelectorAll('#singupForm input');
 
-const expresiones = {
+const regexs = {
     nickname: /^[a-zA-Z0-9\_\-]{4,20}$/,
-    name: /^[a-zA-Z]{1,40}$/,
-    surnames: /^[a-zA-Z]{1,40}$/,
-    email:/^[a-zA-Z0-9_.+-]+@[a-z-A-Z0-9]+\.[a-zA-Z0-9-.]+$/, 
-    password: /^.{4,20}$/,
-    password2: /^.{4,20}$/,
+    name: /^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$/,
+    surnames: /^([A-ZÁÉÍÓÚ a-zñáéíóú]{1,})+$/,
+    email: /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+    phone: /^[679]{1}[0-9]{8}/,
+    password: /(?=(.*[0-9]))(?=.*[a-z])(?=(.*[A-Z]))(?=(.*)).{4,20}/
 }
-var correcto = true;
-const validarFormulario= (e)=>{
+
+const correct = false;
+
+const form_validation= (e)=>{
     switch (e.target.name) {
+
         case "nickname":
-            if(expresiones.nickname.test(e.target.value)){
-                document.getElementById('nickname').classList.remove('input_incorrecto');
-                document.getElementById('nickname').classList.add('input_correcto');
-                correcto = true
-            }else{
-                document.getElementById('nickname').classList.add('input_incorrecto');
-                document.getElementById('nickname').classList.remove('input_correcto');
-                correcto = false;
+            if (regexs.nickname.test(e.target.value)) {
+                document.getElementById('nickname').classList.remove('input_error');
+                document.getElementById('nickname-error').classList.add('hidden');
+                correct = true;
+            } else if (e.target.value === '') {
+                document.getElementById('nickname').classList.remove('input_error');
+                document.getElementById('nickname-error').classList.add('hidden');
+                correct = false;
+            } else {
+                document.getElementById('nickname').classList.add('input_error');
+                document.getElementById('nickname-error').classList.remove('hidden');
+                correct = false;
             }
             break;  
+
         case "email":
-            if(expresiones.email.test(e.target.value)){
-                document.getElementById('email').classList.remove('input_incorrecto');
-                document.getElementById('email').classList.add('input_correcto');
-                correcto = true
-            }else{
-                document.getElementById('email').classList.add('input_incorrecto');
-                document.getElementById('email').classList.remove('input_correcto');
-                correcto = false;
+            if (regexs.email.test(e.target.value)){
+                document.getElementById('email').classList.remove('input_error');
+                document.getElementById('email-error').classList.add('hidden');
+                correct = true;
+            } else if (e.target.value === '') {
+                document.getElementById('email').classList.remove('input_error');
+                document.getElementById('email-error').classList.add('hidden');
+                correct = false;
+            } else {
+                document.getElementById('email').classList.add('input_error');
+                document.getElementById('email-error').classList.remove('hidden');
+                correct = false;
             }
-            break;  
+            break; 
+
         case "name":
-            if(expresiones.name.test(e.target.value)){
-                document.getElementById('name').classList.remove('input_incorrecto');
-                document.getElementById('name').classList.add('input_correcto');
-                correcto = true;
-            }else{
-                document.getElementById('name').classList.add('input_incorrecto');
-                document.getElementById('name').classList.remove('input_correcto');
-                correcto = false;
+            if (regexs.name.test(e.target.value)) {
+                document.getElementById('name').classList.remove('input_error');
+                document.getElementById('name-error').classList.add('hidden');
+                correct = true;
+            } else if (e.target.value === '') {
+                document.getElementById('name').classList.remove('input_error');
+                document.getElementById('name-error').classList.add('hidden');
+                correct = false;
+            } else {
+                document.getElementById('name').classList.add('input_error');
+                document.getElementById('name-error').classList.remove('hidden');
+                correct = false;
             }
             break;  
+
         case "surnames":
-            if(expresiones.surnames.test(e.target.value)){
-                document.getElementById('surnames').classList.remove('input_incorrecto');
-                document.getElementById('surnames').classList.add('input_correcto');
-                correcto = true;
-            }else{
-                document.getElementById('surnames').classList.add('input_incorrecto');
-                document.getElementById('surnames').classList.remove('input_correcto');
-                correcto = false;
+            if (regexs.surnames.test(e.target.value)) {
+                document.getElementById('surnames').classList.remove('input_error');
+                document.getElementById('surnames-error').classList.add('hidden');
+                correct = true;
+            } else if (e.target.value === '') {
+                document.getElementById('surnames').classList.remove('input_error');
+                document.getElementById('surnames-error').classList.add('hidden');
+                correct = false;
+            } else {
+                document.getElementById('surnames').classList.add('input_error');
+                document.getElementById('surnames-error').classList.remove('hidden');
+                correct = false;
             }
             break;  
+
+        case "phone":
+            if (regexs.phone.test(e.target.value)) {
+                document.getElementById('phone').classList.remove('input_error');
+                document.getElementById('phone-error').classList.add('hidden');
+                correct = true;
+            } else if (e.target.value === '') {
+                document.getElementById('phone').classList.remove('input_error');
+                document.getElementById('phone-error').classList.add('hidden');
+                correct = false;
+            } else {
+                document.getElementById('phone').classList.add('input_error');
+                document.getElementById('phone-error').classList.remove('hidden');
+                correct = false;
+            }
+            break;
+
         case "date":
-            if(e.target.value ===""){
+            if (e.target.value === '') {
                 document.getElementById('date').classList.add('input_incorrecto');
                 document.getElementById('date').classList.remove('input_correcto');
-                correcto = true;
-            }else{
-                
+                correct = true;
+            } else {
                 document.getElementById('date').classList.remove('input_incorrecto');
                 document.getElementById('date').classList.add('input_correcto');
-                correcto = false;
+                correct = false;
             }
-            break;  
-        case "password":
-            if(expresiones.password.test(e.target.value)){
-                document.getElementById('password').classList.remove('input_incorrecto');
-                document.getElementById('password').classList.add('input_correcto');
-                correcto = true;
-            }else{
-                document.getElementById('password').classList.add('input_incorrecto');
-                document.getElementById('password').classList.remove('input_correcto');
-                correcto = false;
-            }
-            break;  
-        case "password2":
-            if(expresiones.password2.test(e.target.value)){
-                document.getElementById('password2').classList.remove('input_incorrecto');
-                document.getElementById('password2').classList.add('input_correcto');
-                correcto = true;
-            }else{
-                document.getElementById('password2').classList.add('input_incorrecto');
-                document.getElementById('password2').classList.remove('input_correcto');
-                correcto = false;
-            }
-            break;  
+            break;
 
+        case "password":
+            if (regexs.password.test(e.target.value)) {
+                document.getElementById('password').classList.remove('input_error');
+                document.getElementById('password-error').classList.add('hidden');
+                correct = true;
+            } else if (e.target.value === '') {
+                document.getElementById('password').classList.remove('input_error');
+                document.getElementById('password-error').classList.add('hidden');
+                correct = false;
+            } else {
+                document.getElementById('password').classList.add('input_error');
+                document.getElementById('password-error').classList.remove('hidden');
+                correct = false;
+            }
+            break;
+
+        case "password2":
+            let password = document.getElementById('password');
+            if (password.value === e.target.value) {
+                document.getElementById('password2').classList.remove('input_error');
+                document.getElementById('password2-error').classList.add('hidden');
+                correct = true;
+            } else if (e.target.value === '') {
+                document.getElementById('password2').classList.remove('input_error');
+                document.getElementById('password2-error').classList.add('hidden');
+                correct = false;
+            } else {
+                document.getElementById('password2').classList.add('input_error');
+                document.getElementById('password2-error').classList.remove('hidden');
+                correct = false;
+            }
+            break;
     }
 }
 
 inputs.forEach((input) => {
-input.addEventListener('keyup', validarFormulario);
-input.addEventListener('blur', validarFormulario);
-
+    input.addEventListener('keyup', form_validation);
+    input.addEventListener('blur', form_validation);
 });
 
-formulario.addEventListener('submit', (e) => {
-    if(correcto !=true){
-    e.preventDefault();
-}
+form.addEventListener('submit', (e) => {
+    const termns = document.getElementById('termns');
+    
+    if(!correct && !termns.checked) {
+        e.preventDefault();
+        document.getElementById('form-error').classList.remove('hidden');
+        setTimeout(() => {
+			document.getElementById('form-error').classList.add('hidden');
+		}, 3500);
+    }
 });
-
-
