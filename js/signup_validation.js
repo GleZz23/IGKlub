42f56f177e90cd5bee407648ee6949d229f03624
Andriucm <andreimihalatxe@gmail.com>
2022-10-19 14:08:42 +0200

    validacion registro terminado

--- js/signup_validation.js
+++ js/signup_validation.js
@@ -9,70 +9,85 @@ const expresiones = {
     password: /^.{4,20}$/,
     password2: /^.{4,20}$/,
 }
+var correcto = true;
 const validarFormulario= (e)=>{
     switch (e.target.name) {
         case "nickname":
             if(expresiones.nickname.test(e.target.value)){
                 document.getElementById('nickname').classList.remove('input_incorrecto');
                 document.getElementById('nickname').classList.add('input_correcto');
+                correcto = true
             }else{
                 document.getElementById('nickname').classList.add('input_incorrecto');
                 document.getElementById('nickname').classList.remove('input_correcto');
+                correcto = false;
             }
             break;  
         case "email":
             if(expresiones.email.test(e.target.value)){
                 document.getElementById('email').classList.remove('input_incorrecto');
                 document.getElementById('email').classList.add('input_correcto');
+                correcto = true
             }else{
                 document.getElementById('email').classList.add('input_incorrecto');
                 document.getElementById('email').classList.remove('input_correcto');
+                correcto = false;
             }
             break;  
         case "name":
             if(expresiones.name.test(e.target.value)){
                 document.getElementById('name').classList.remove('input_incorrecto');
                 document.getElementById('name').classList.add('input_correcto');
+                correcto = true;
             }else{
                 document.getElementById('name').classList.add('input_incorrecto');
                 document.getElementById('name').classList.remove('input_correcto');
+                correcto = false;
             }
             break;  
         case "surnames":
             if(expresiones.surnames.test(e.target.value)){
                 document.getElementById('surnames').classList.remove('input_incorrecto');
                 document.getElementById('surnames').classList.add('input_correcto');
+                correcto = true;
             }else{
                 document.getElementById('surnames').classList.add('input_incorrecto');
                 document.getElementById('surnames').classList.remove('input_correcto');
+                correcto = false;
             }
             break;  
         case "date":
             if(e.target.value ===""){
                 document.getElementById('date').classList.add('input_incorrecto');
                 document.getElementById('date').classList.remove('input_correcto');
+                correcto = true;
             }else{
                 
                 document.getElementById('date').classList.remove('input_incorrecto');
                 document.getElementById('date').classList.add('input_correcto');
+                correcto = false;
             }
             break;  
         case "password":
             if(expresiones.password.test(e.target.value)){
                 document.getElementById('password').classList.remove('input_incorrecto');
                 document.getElementById('password').classList.add('input_correcto');
+                correcto = true;
             }else{
                 document.getElementById('password').classList.add('input_incorrecto');
                 document.getElementById('password').classList.remove('input_correcto');
+                correcto = false;
             }
             break;  
         case "password2":
             if(expresiones.password2.test(e.target.value)){
                 document.getElementById('password2').classList.remove('input_incorrecto');
                 document.getElementById('password2').classList.add('input_correcto');
+                correcto = true;
             }else{
                 document.getElementById('password2').classList.add('input_incorrecto');
                 document.getElementById('password2').classList.remove('input_correcto');
+                correcto = false;
             }
             break;  
 
@@ -86,7 +101,9 @@ input.addEventListener('blur', validarFormulario);
 });
 
 formulario.addEventListener('submit', (e) => {
+    if(correcto !=true){
     e.preventDefault();
+}
 });
 
 
