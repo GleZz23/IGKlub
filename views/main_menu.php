2d257e6ca68edb13e1c1b58b2054e3f58ccf261e
GleZz23 <ikerglez110@gmail.com>
2022-10-24 10:57:21 +0200

    -

--- views/main_menu.php
+++ views/main_menu.php
@@ -52,67 +52,8 @@
       $query->execute();
       $results = $query->fetchAll();
 
-    
-
+      
+    ?>
     </main>
-    <footer class="footer">
-            <div class="footer__addr">
-              <h1 class="footer__logo">Guri buruz</h1>
-                  
-              <h2>Kontaktua</h2>
-              
-              <address>
-                © I.E.S. Miguel de Unamuno B.H.I.<br>
-                    
-                <a class="footer__btn" href="mailto:leireirakas21@gmail.com">Gmail</a>
-              </address>
-            </div>
-            
-            <ul class="footer__nav">
-              <li class="nav__item">
-                <h2 class="nav__title">Edukiak</h2>
-          
-                <ul class="nav__ul">
-                  <li>
-                    <a href="#">Profila</a>
-                  </li>
-          
-                  <li>
-                    <a href="#">Liburuak</a>
-                  </li>
-                      
-                  <li>
-                    <a href="#">Iritzia</a>
-                  </li>
-                </ul>
-              </li>
-              
-              
-              <li class="nav__item">
-                <h2 class="nav__title">Kredituak</h2>
-                
-                <ul class="nav__ul">
-                  <li>
-                    <a href="https://fptxurdinaga.hezkuntza.net/es/web/Guest">FP Txurdinaga</a>
-                  </li>
-                  
-                  <li>
-                    <p>Txurdinaga LHII Lanbide Heziketako azken ikasturteko 2º ikasleek diseinatu dute webgune hau (Andrei,Ciprian,Iker,Iñigo).</p>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            
-            <div class="legal">
-              <p>&copy; 2022 IGKlub. All rights reserved.</p>
-              
-              <div class="legal__links">
-                <span><span class="heart"></span> 
-                Bigarren Hezkuntzako gazte askok maite dute irakurketa. Hala ere, liburu-dendetan hainbeste liburu daude non ez dakigun nondik hasi. Webgune honetan gazteentzako eta ez hain gazteentzako liburuak daude: arrakastatsuenak, baita gustatu ez zaizkigunak ere. Bilatu eta gozatu!</span>
-              </div>
-            </div>
-          </footer>
-
-    
 </body>
 </html>
