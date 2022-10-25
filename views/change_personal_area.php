16a502990efe012f839d315b399909ee101f95fc
GleZz23 <ikerglez110@gmail.com>
2022-10-25 13:25:35 +0200

    -

--- views/change_personal_area.php
+++ views/change_personal_area.php
@@ -1,41 +1,37 @@
 <?php
-include_once('../templates/head.php');
-include_once('../modules/connection.php');
-session_start();
-// FILTROS
+  include_once('../templates/head.php');
+  include_once('../modules/connection.php');
+  session_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $signup = true;
-  $password = $_POST["password"];
-  $password = password_hash($password, PASSWORD_DEFAULT);
+  $password_error = false;
 
-  $query = $miPDO->prepare('SELECT contrasena FROM usuario WHERE nickname=:nickname');
-  $query->execute(['nickname' => $_SESSION['nickname']]);
-  $results = $query->fetch();
+  if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $change = true;
 
-  // Si la contraseña es igual
-  if ($password === $results) {
+    // Consulto la contraseña en la BBDD
+    $query = $miPDO->prepare('SELECT contrasena FROM usuario WHERE nickname=:nickname');
+    $query->execute(['nickname' => $_POST["password"]]);
+    $results = $query->fetch();
+
+    // Compruebo que la contraseña introducida es igual a la contraseña de la BBDD
+    if (password_verify($_POST['password'], $results['contrasena'])) {
+      $change = false;
       $password_error = true;
-      $signup = false;
     }
-    if ($signup) {
-      // Inserto el usuario en la base de datos
-      $query = $miPDO->prepare('UPDATE usuario (contrasena) VALUES (:password) WHERE nickname=:nickname');
-      $query->execute(['nickname' => $_SESSION['nickname'],'password' => $password]);
-  }
-  
-}
-?>
 
-  <!-- <script src="../src/js/hamburgesa.js" defer></script> -->
-<html>
-  <head>
-  <script src="../src/js/profile_validation.js" defer></script>
+    // Cambio la contraseña en la BBDD
+    if ($change) {
+      $query = $miPDO->prepare('UPDATE usuario (contrasena) VALUES (:password) WHERE nickname=:nickname');
+      $query->execute(['nickname' => $_SESSION['nickname'],'password' => $_POST["password"]]);
+    }
+  }
+?>
+  <script src="../src/js/change_password.js" defer></script>
   <script src="../src/js/profile.js" defer></script>
   <link rel="stylesheet" href="../styles/personal_area.css">
   
-  <title>Hasiera | IGKlub</title>
-  </head>
+  <title> | IGKlub</title>
+</head>
 <body>
   <header>
     <figure>
@@ -72,27 +68,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
           <input type="password" name="password" id="password" placeholder="Pasahitza">
           <input type="password" name="password2" id="password2" placeholder="Pasahitza egiaztatu">
         </div>
-        <!-- Error: Contraseñas -->
-    <div class="error hidden" id="password-error">
-      <i class="fa-solid fa-circle-exclamation"></i>
-      <p>Pasahitzak 4 karaktere izan behar ditu gutxienez eta letra larria, minuskula eta zenbaki bat izan behar ditu.</p>
-    </div>
-    <div class="error hidden" id="password2-error">
-      <i class="fa-solid fa-circle-exclamation"></i>
-      <p>Bi pasahitzek berdinak izan behar dira.</p>
-    </div>
-    <?php
-    // if ($password_error) {
-    //   echo '<div class="error">
-    //           <i class="fa-solid fa-circle-exclamation"></i>
-    //           <p>Pasahitza berdina sartzen ari zara.</p>
-    //         </div>';
-    // }
-    ?>
       </div>
-        <button>Gorde</button>
+      <!-- Error: Contraseñas -->
+      <div class="error hidden" id="password-error">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        <p>Pasahitzak 4 karaktere izan behar ditu gutxienez eta letra larria, minuskula eta zenbaki bat izan behar ditu.</p>
+      </div>
+      <div class="error hidden" id="password2-error">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        <p>Bi pasahitzek berdinak izan behar dira.</p>
+      </div>
+      <?php
+        if ($password_error) {
+          echo '<div class="error">
+                  <i class="fa-solid fa-circle-exclamation"></i>
+                  <p>Pasahitza berdina sartzen ari zara.</p>
+                </div>';
+        }
+      ?>
+      <button>Pasahitza aldatu</button>
     </form>
   </main>
-  
 </body>
 </html>
