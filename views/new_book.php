c072b81c9f1d97412ad85891d69978618d4baa74
Andriucm <andreimihalatxe@gmail.com>
2022-10-26 13:28:26 +0200

    Agregar nuevo libro

--- views/new_book.php
+++ views/new_book.php
@@ -9,7 +9,8 @@ session_start();
 
 <head>
     <script src="../src/js/profile.js" defer></script>
-    <link rel="stylesheet" href="../styles/personal_area.css">
+    <script src="../src/js/new_book_validation.js" defer></script>
+    <link rel="stylesheet" href="../styles/new_book.css">
     <title>Hasiera | IGKlub</title>
 </head>
 
@@ -24,23 +25,88 @@ session_start();
             </button>
             <aside class="profile">
                 <?php
-    if ($_SESSION['role'] === 'ikasle') {
-        echo '<h1>' . $_SESSION['nickname'] . '</h1>';
-        echo '<h1><a href="../views/personal_area.php">Area Pertsonala</a> </h1>';
-        echo '<h1><a href="../views/main_menu.php">Liburutegia</a></h1>';
-    } else
-        echo '<h1>' . $_SESSION['nickname'] . '</h1>';
-    echo '<h1><a href="../views/personal_area.php">Area Pertsonala</a> </h1>';
-    echo '<h1><a href="../views/main_menu.php">Liburutegia</a></h1>';
-    echo '<h1><a href="../views/class.php">Gela</a></h1>';
-    echo '<h1><a href="../views/requests.php">Eskaerak</a></h1>';
-    ?>
+                if ($_SESSION['role'] === 'ikasle') {
+                    echo '<h1>' . $_SESSION['nickname'] . '</h1>';
+                    echo '<h1><a href="../views/personal_area.php">Area Pertsonala</a> </h1>';
+                    echo '<h1><a href="../views/main_menu.php">Liburutegia</a></h1>';
+                } else
+                    echo '<h1>' . $_SESSION['nickname'] . '</h1>';
+                echo '<h1><a href="../views/personal_area.php">Area Pertsonala</a> </h1>';
+                echo '<h1><a href="../views/main_menu.php">Liburutegia</a></h1>';
+                echo '<h1><a href="../views/class.php">Gela</a></h1>';
+                echo '<h1><a href="../views/requests.php">Eskaerak</a></h1>';
+                ?>
                 <!-- PERFIL -->
             </aside>
         </section>
     </header>
     <main>
-        
+        <form action="" id="add_book_form">
+            <div id="add_book_form_header">
+                <h1>Liburu barria sartu</h1>
+            </div>
+            <div id="add_book_form_container">
+                <!-- Titulo -->
+                <div class="input_container">
+                    <input type="text" id="title" name="title" placeholder="Titulua">
+                </div>
+                <!-- Error titulo -->
+                <div class="error hidden" id="title-error">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <p>Titulua sartzea beharrezkoa da.</p>
+                </div>
+                <!--Escritor  -->
+                <div class="input_container">
+                    <input type="text" id="writter" name="writter" placeholder="Idazlea">
+                </div>
+                <!-- Error escritor -->
+                <div class="error hidden" id="writter-error">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <p>Idazlea sartzea beharrezkoa da.</p>
+                </div>
+                <!-- Sinopsis -->
+                <div class="input_container">
+                    <input type="text" id="sinopsis" name="sinopsis" placeholder="Sinopsia">
+                </div>
+                <!-- Error sinopsis -->
+                <div class="error hidden" id="sinopsis-error">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <p>Sinopsia sartzea beharrezkoa da.</p>
+                </div>
+                <!--Idioma  -->
+                <div class="input_container">
+                    <input type="text" id="language" name="language" placeholder="Hizkuntza">
+                </div>
+                <!-- Error idioma -->
+                <div class="error hidden" id="language-error">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <p>Hizkuntza sartzea beharrezkoa da.</p>
+                </div>
+                <!-- Formato -->
+                <div class="input_container">
+                    <label>Formatua </label><select id="format" name="format">
+                        <option value="Nobela">Nobela</option>
+                        <option value="Komikia">Komikia</option>
+                        <option value="Nobela grafikoa">Nobela grafikoa</option>
+                        <option value="Manga">Manga</option>
+                    </select>
+                </div>
+                <!-- Error formato -->
+                <div class="error hidden" id="format-error">
+                    <i class="fa-solid fa-circle-exclamation"></i>
+                    <p>Formatua sartzea beharrezkoa da.</p>
+                </div>
+                <!-- Imagen  -->
+                <div class="input_container">
+                    <label>Irudia </label><input type="file" id="image" name="image" accept="image/jpg" required>
+                </div>
+                <!-- Etiqueta -->
+                <div class="input_container">
+                    <input type="text" id="label" name="label">
+                </div>
+            </div>
+            <button>Konfirmatu</button>
+        </form>
     </main>
 
 </body>
