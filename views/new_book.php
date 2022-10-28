7e744097638a872d8bbfa21325b19966b5d8098b
GleZz23 <ikerglez110@gmail.com>
2022-10-28 14:15:05 +0200

    -

--- views/new_book.php
+++ views/new_book.php
@@ -1,17 +1,12 @@
 <?php
-include_once('../templates/head.php');
-include_once('../modules/connection.php');
-session_start();
-// FILTROS
+    include_once('../templates/head.php');
+    include_once('../modules/connection.php');
+    session_start();
 ?>
-
-<!-- <script src="../src/js/hamburgesa.js" defer></script> -->
-
-<head>
     <script src="../src/js/profile.js" defer></script>
     <script src="../src/js/new_book_validation.js" defer></script>
     <link rel="stylesheet" href="../styles/new_book.css">
-    <title>Hasiera | IGKlub</title>
+    <title>Igo liburu bat | IGKlub</title>
 </head>
 
 <body>
@@ -41,72 +36,82 @@ session_start();
         </section>
     </header>
     <main>
-        <form action="" id="add_book_form">
-            <div id="add_book_form_header">
-                <h1>Liburu berria sartu</h1>
-            </div>
-            <div id="add_book_form_container">
-                <!-- Titulo -->
-                <div class="input_container">
-                    <input type="text" id="title" name="title" placeholder="Titulua">
-                </div>
-                <!-- Error titulo -->
-                <div class="error hidden" id="title-error">
-                    <i class="fa-solid fa-circle-exclamation"></i>
-                    <p>Titulua sartzea beharrezkoa da.</p>
-                </div>
-                <!--Escritor  -->
-                <div class="input_container">
-                    <input type="text" id="writter" name="writter" placeholder="Idazlea">
-                </div>
-                <!-- Error escritor -->
-                <div class="error hidden" id="writter-error">
-                    <i class="fa-solid fa-circle-exclamation"></i>
-                    <p>Idazlea sartzea beharrezkoa da.</p>
-                </div>
-                <!-- Sinopsis -->
-                <div class="input_container">
-                    <input type="text" id="sinopsis" name="sinopsis" placeholder="Sinopsia">
-                </div>
-                <!-- Error sinopsis -->
-                <div class="error hidden" id="sinopsis-error">
-                    <i class="fa-solid fa-circle-exclamation"></i>
-                    <p>Sinopsia sartzea beharrezkoa da.</p>
-                </div>
-                <!--Idioma  -->
-                <div class="input_container">
-                    <input type="text" id="language" name="language" placeholder="Hizkuntza">
-                </div>
-                <!-- Error idioma -->
-                <div class="error hidden" id="language-error">
-                    <i class="fa-solid fa-circle-exclamation"></i>
-                    <p>Hizkuntza sartzea beharrezkoa da.</p>
-                </div>
-                <!-- Formato -->
-                <div class="input_container">
-                    <label>Formatua </label><select id="format" name="format">
-                        <option value="Nobela">Nobela</option>
-                        <option value="Komikia">Komikia</option>
-                        <option value="Nobela grafikoa">Nobela grafikoa</option>
-                        <option value="Manga">Manga</option>
-                    </select>
-                </div>
-                <!-- Error formato -->
-                <div class="error hidden" id="format-error">
-                    <i class="fa-solid fa-circle-exclamation"></i>
-                    <p>Formatua sartzea beharrezkoa da.</p>
-                </div>
-                <!-- Imagen 
-                <div class="input_container">
-                    <label>Irudia </label><input type="file" id="image" name="image" accept="image/jpg" required>
-                </div> -->
-                <!-- Etiqueta -->
-                <div class="input_container">
-                    <input type="text" id="label" name="label">
-                </div>
-            </div>
-            <button>Konfirmatu</button>
-        </form>
+    <form id="singupForm" action="" method="post">
+    <h1>Igo liburu bat</h1>
+    <!-- Titulo del libro -->
+    <div class="input-container">
+        <i class="fa-solid fa-heading"></i>
+        <input type="text" name="title" id="title" placeholder="Izenburua" autofocus value="<?php if (isset($_REQUEST['nickname'])) echo $_REQUEST['nickname'] ?>">
+    </div>
+    <!-- Error: Titulo del libro -->
+    <div class="error hidden" id="title-error">
+      <i class="fa-solid fa-circle-exclamation"></i>
+      <p>.</p>
+    </div>
+    <!-- Escritor -->
+    <div class="input-container">
+        <i class="fa-solid fa-feather"></i>
+        <input type="text" name="writter" id="writter" placeholder="Idazlea" value="<?php if (isset($_REQUEST['email'])) echo $_REQUEST['email'] ?>">
+    </div>
+    <!-- Error: Escritor -->
+    <div class="error hidden" id="writter-error">
+      <i class="fa-solid fa-circle-exclamation"></i>
+      <p>.</p>
+    </div>
+    <!-- Portada -->
+    <div class="input-container">
+        <i class="fa-solid fa-file-image"></i>
+        <input type="file" name="cover" id="cover" placeholder="Liburuaren azala">
+    </div>
+    <!-- Error: Portada -->
+    <div class="error hidden" id="cover-error">
+      <i class="fa-solid fa-circle-exclamation"></i>
+      <p>.</p>
+    </div>
+    <!-- Idiaoma -->
+    <div class="input-container">
+        <i class="fa-solid fa-language"></i>
+        <select name="language" id="language">
+            <option value="-" selected>Hizkuntza</option>
+        <?php
+            $query = $miPDO->prepare('SELECT * FROM idioma ORDER BY id_idioma ASC');
+            $query->execute();
+            $results = $query->fetchAll();
+
+            foreach ($results as $position => $language) {
+                echo '<option value="'.$language['id_idioma'].'">'.$language['nombre'].'</option>';
+            }
+        ?>
+        </select>
+    </div>
+    <!-- Error: Formato -->
+    <div class="error hidden" id="language-error">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        <p>Aukeratu eskola bat.</p>
+    </div>
+    <!-- Formato -->
+    <div class="input-container">
+        <i class="fa-solid fa-rectangle-list"></i>
+        <select name="format" id="format">
+            <option value="-" selected>Formatua</option>
+            <option value="Nobela">Nobela</option>
+            <option value="Nobela grafikoa">Nobela grafikoa</option>
+            <option value="Komikia">Komikia</option>
+            <option value="Manga">Manga</option>
+        </select>
+    </div>
+    <!-- Error: Formato -->
+    <div class="error hidden" id="format-error">
+        <i class="fa-solid fa-circle-exclamation"></i>
+        <p>Aukeratu eskola bat.</p>
+    </div>
+    <!-- Sinopsis -->
+    <div class="input-container">
+        <i class="fa-solid fa-marker"></i>
+        <textarea id="sinopsis" name="sinopsis" placeholder="Sinopsia" autofocus required autocomplete="off" maxlength="2300"></textarea>
+    </div>
+    <button>Igo liburua</button>
+  </form>
     </main>
 
 </body>
