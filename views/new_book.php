e079901261b11c827e60894e7abb52686866db50
Inigocampo08 <inigocampo08@gmail.com>
2022-11-02 14:26:03 +0100

    grupo

--- views/new_book.php
+++ views/new_book.php
@@ -1,5 +1,4 @@
 <?php
-<<<<<<< HEAD
     include_once('../templates/head.php');
     include_once('../modules/connection.php');
     session_start();
@@ -26,15 +25,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 
 }
-=======
-include_once('../templates/head.php');
-include_once('../modules/connection.php');
-session_start();
->>>>>>> fb827143a47a2a9edc38e7d265dde5752b6cadc1
 ?>
 <script src="../src/js/profile.js" defer></script>
-<script src="../src/js/new_book_validation.js" defer></script>
-<link rel="stylesheet" href="../styles/new_book.css">
+<link rel="stylesheet" href="../styles/new_class.css">
 <title>Igo liburu bat | IGKlub</title>
 </head>
 
@@ -64,7 +57,6 @@ session_start();
         </section>
     </header>
     <main>
-<<<<<<< HEAD
     <form id="singupForm" action="" method="post">
     <h1>Igo liburu bat</h1>
     <!-- Titulo del libro -->
@@ -106,59 +98,11 @@ session_start();
             $query = $miPDO->prepare('SELECT * FROM idioma ORDER BY id_idioma ASC');
             $query->execute();
             $results = $query->fetchAll();
-=======
-        <form id="singupForm" action="" method="post">
-            <h1>Igo liburu bat</h1>
-            <!-- Titulo del libro -->
-            <div class="input-container">
-                <i class="fa-solid fa-heading"></i>
-                <input type="text" name="title" id="title" placeholder="Izenburua" autofocus
-                    value="<?php if (isset($_REQUEST['nickname']))
-            echo $_REQUEST['nickname'] ?>">
-            </div>
-            <!-- Error: Titulo del libro -->
-            <div class="error hidden" id="title-error">
-                <i class="fa-solid fa-circle-exclamation"></i>
-                <p>.</p>
-            </div>
-            <!-- Escritor -->
-            <div class="input-container">
-                <i class="fa-solid fa-feather"></i>
-                <input type="text" name="writter" id="writter" placeholder="Idazlea"
-                    value="<?php if (isset($_REQUEST['email']))
-            echo $_REQUEST['email'] ?>">
-            </div>
-            <!-- Error: Escritor -->
-            <div class="error hidden" id="writter-error">
-                <i class="fa-solid fa-circle-exclamation"></i>
-                <p>.</p>
-            </div>
-            <!-- Portada -->
-            <div class="input-container">
-                <i class="fa-solid fa-file-image"></i>
-                <input type="file" name="cover" id="cover" placeholder="Liburuaren azala">
-            </div>
-            <!-- Error: Portada -->
-            <div class="error hidden" id="cover-error">
-                <i class="fa-solid fa-circle-exclamation"></i>
-                <p>.</p>
-            </div>
-            <!-- Idiaoma -->
-            <div class="input-container">
-                <i class="fa-solid fa-language"></i>
-                <select name="language" id="language">
-                    <option value="-" selected>Hizkuntza</option>
-                    <?php
-        $query = $miPDO->prepare('SELECT * FROM idioma ORDER BY id_idioma ASC');
-        $query->execute();
-        $results = $query->fetchAll();
->>>>>>> fb827143a47a2a9edc38e7d265dde5752b6cadc1
 
         foreach ($results as $position => $language) {
             echo '<option value="' . $language['id_idioma'] . '">' . $language['nombre'] . '</option>';
         }
         ?>
-<<<<<<< HEAD
         </select>
     </div>
     <!-- Error: Formato -->
@@ -193,40 +137,6 @@ session_start();
 
   </form>
 </main>
-=======
-                </select>
-            </div>
-            <!-- Error: Formato -->
-            <div class="error hidden" id="language-error">
-                <i class="fa-solid fa-circle-exclamation"></i>
-                <p>Aukeratu eskola bat.</p>
-            </div>
-            <!-- Formato -->
-            <div class="input-container">
-                <i class="fa-solid fa-rectangle-list"></i>
-                <select name="format" id="format">
-                    <option value="-" selected>Formatua</option>
-                    <option value="Nobela">Nobela</option>
-                    <option value="Nobela grafikoa">Nobela grafikoa</option>
-                    <option value="Komikia">Komikia</option>
-                    <option value="Manga">Manga</option>
-                </select>
-            </div>
-            <!-- Error: Formato -->
-            <div class="error hidden" id="format-error">
-                <i class="fa-solid fa-circle-exclamation"></i>
-                <p>Aukeratu eskola bat.</p>
-            </div>
-            <!-- Sinopsis -->
-            <div class="input-container">
-                <i class="fa-solid fa-marker"></i>
-                <textarea id="sinopsis" name="sinopsis" placeholder="Sinopsia" autofocus required autocomplete="off"
-                    maxlength="2300"></textarea>
-            </div>
-            <button>Igo liburua</button>
-        </form>
-    </main>
->>>>>>> fb827143a47a2a9edc38e7d265dde5752b6cadc1
 
 </body>
 
--- views/new_group.php
+++ views/new_group.php
@@ -2,11 +2,62 @@
 include_once('../templates/head.php');
 include_once('../modules/connection.php');
 session_start();
+
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+  $nombre = $_REQUEST['title'];
+  $colegio = $_REQUEST['school'];
+  $nivel = $_REQUEST['level'];
+  $curso = $_REQUEST['curso'];
+
+
+
+  $query = $miPDO->prepare('INSERT INTO Grupo (nombre, nivel, curso) VALUES (:nombre, :nivel, :curso)');
+  $query->execute(['nombre' => $nombre,'nivel' => $nivel,'curso' => $curso]);
+
+  $query = $miPDO->prepare('SELECT codigo FROM grupo WHERE nombre = :nombre');
+  $query->execute(['nombre' => $nombre]);
+  $id = $query->fetch();
+
+  $query = $miPDO->prepare('INSERT INTO solicitud_grupo (nickname, cod_grupo, estado) VALUES (:nickname, :id,"espera")');
+  $query->execute(['nickname' => $_SESSION['nickname'], 'cod_grupo' => $id['codigo']]);
+
+
+}
 ?>
-  <link rel="stylesheet" href="../styles/new_class.css">
+<script src="../src/js/profile.js" defer></script>
+<script src="../src/js/new_book_validation.js" defer></script>
+<link rel="stylesheet" href="../styles/new_book.css">
   <title>Gela sortu | IGKlub</title>
   </head>
 <body>
+<header>
+        <figure>
+            <img src="../src/img/logo/logo.png">
+        </figure>
+        <section>
+            <button id="profile">
+                <i class="fa-solid fa-bars"></i>
+            </button>
+            <aside class="profile">
+                <?php
+                if ($_SESSION['role'] === 'ikasle') {
+                    echo '<h1>' . $_SESSION['nickname'] . '</h1>';
+                    echo '<h1><a href="../views/personal_area.php">Area Pertsonala</a> </h1>';
+                    echo '<h1><a href="../views/main_menu.php">Liburutegia</a></h1>';
+                } else
+                    echo '<h1>' . $_SESSION['nickname'] . '</h1>';
+                echo '<h1><a href="../views/personal_area.php">Area Pertsonala</a> </h1>';
+                echo '<h1><a href="../views/main_menu.php">Liburutegia</a></h1>';
+                echo '<h1><a href="../views/class.php">Gela</a></h1>';
+                echo '<h1><a href="../views/requests.php">Eskaerak</a></h1>';
+                ?>
+            </aside>
+        </section>
+    </header>
+    <main>
+
 <form action="" method="post">
     <h1>Talde bat sortu</h1>
     <!-- Nombre del grupo -->
@@ -38,7 +89,7 @@ session_start();
     <!-- Nivel -->
     <div class="input-container">
       <i class="fa-solid fa-heading"></i>
-      <input type="text" name="level" id="level" placeholder="" value="<?php if (isset($_REQUEST['nickname'])) echo $_REQUEST['nickname'] ?>">
+      <input type="text" name="level" id="level" placeholder="Maila" value="<?php if (isset($_REQUEST['nickname'])) echo $_REQUEST['nickname'] ?>">
     </div>
     <!-- Error: Nivel -->
     <div class="error hidden" id="title-error">
@@ -48,13 +99,14 @@ session_start();
     <!-- Curso -->
     <div class="input-container">
       <i class="fa-solid fa-heading"></i>
-      <input type="text" name="title" id="title" placeholder="Taldearen izena" value="<?php if (isset($_REQUEST['nickname'])) echo $_REQUEST['nickname'] ?>">
+      <input type="text" name="curso" id="title" placeholder="Kurtzoa" value="<?php if (isset($_REQUEST['nickname'])) echo $_REQUEST['nickname'] ?>">
     </div>
     <!-- Error: Curso -->
     <div class="error hidden" id="title-error">
       <i class="fa-solid fa-circle-exclamation"></i>
       <p>.</p>
     </div>
-    <button>Igo liburua</button>
+    <button>Gorde gela</button>
   </form>
+  </main>
 </body>
