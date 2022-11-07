d4a4a5b9ade971786e4c5f7dff67981b85785798
GleZz23 <ikerglez110@gmail.com>
2022-11-07 16:51:16 +0100

    -

--- modules/delete_comment.php
+++ modules/delete_comment.php
@@ -5,6 +5,9 @@
     $query = $miPDO->prepare('DELETE FROM comentario WHERE id_comentario = :id_comentario');
     $query->execute(['id_comentario' => $_GET['id_comment']]);
 
+    $query = $miPDO->prepare('UPDATE valoracion SET comentario = NULL WHERE id_comentario = :id_comentario');
+    $query->execute(['id_comentario' => $_GET['id_comment']]);
+
     header('Location: ../views/book_info.php?liburua='.$_GET['book']);
   }
 ?>
