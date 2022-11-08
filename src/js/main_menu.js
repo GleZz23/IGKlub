a80a8a89438c5ffe8deb921fd4a15e0369c3592e
GleZz23 <ikerglez110@gmail.com>
2022-11-08 09:04:05 +0100

    -

--- src/js/main_menu.js
+++ src/js/main_menu.js
@@ -58,61 +58,6 @@ if (screen.width > 1280) {
   });
 }
 
-<<<<<<< HEAD
-=======
-// PERFIL
-const profileButton = document.querySelector('#profile');
-const profile = document.querySelector('.profile');
-const profileLinks = document.querySelectorAll('.profile a, .profile span');
-const closeProfile = document.querySelector('.close-profile');
-
-profileButton.addEventListener('mouseover', () => {
-  profileButton.style.transform = 'scale(1.1)';
-  profileButton.style.textShadow = ".3rem .3rem .3rem #00000050";
-  profileButton.style.cursor = "pointer";
-});
-
-profileButton.addEventListener('mouseleave', () => {
-  profileButton.style.transform = 'scale(1)';
-  profileButton.style.textShadow = "none";
-});
-
-profileButton.addEventListener('click', () => {
-  profile.style.display = "flex";
-  setTimeout(() => {
-    profile.style.transform = "translateX(0)";
-  }, 10);
-});
-
-if (screen.width > 1280) {
-  profile.addEventListener('mouseleave', () => {
-    profile.style.transform = "translateX(100%)";
-    setTimeout(() => {
-      profile.style.display = "none";
-    }, 500);
-  });
-} else {
-  closeProfile.addEventListener('click', () => {
-    profile.style.transform = "translateX(100%)";
-    setTimeout(() => {
-      profile.style.display = "none";
-    }, 500);
-  });
-}
-
-profileLinks.forEach((link) => {
-  link.addEventListener('mouseover', () => {
-    link.style.color = "black";
-    link.style.transform = "translateX(1rem)";
-  });
-
-  link.addEventListener('mouseout', () => {
-    link.style.color = "gray";
-    link.style.transform = "translateX(0)";
-  });
-});
-
->>>>>>> 9dbf8dad31dd49ba19092236802056c40d2dc6e2
 // MODAL FORMULARIO NUEVO LIBRO
 const newBookButton = document.querySelectorAll('.newBookButton');
 const closeButton = document.querySelector('.closeButton');
