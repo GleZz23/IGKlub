55ea336337487e91e03344d1ea6f531715d7de67
GleZz23 <ikerglez110@gmail.com>
2022-11-08 09:01:53 +0100

    -

--- src/js/main_menu.js
+++ src/js/main_menu.js
@@ -58,69 +58,19 @@ if (screen.width > 1280) {
   });
 }
 
-// PERFIL
-// const profileButton = document.querySelector('#profile');
-// const profile = document.querySelector('.profile');
-// const profileLinks = document.querySelectorAll('.profile a, .profile span');
-// const closeProfile = document.querySelector('.close-profile');
-
-// profileButton.addEventListener('mouseover', () => {
-//   profileButton.style.transform = 'scale(1.1)';
-//   profileButton.style.textShadow = ".3rem .3rem .3rem #00000050";
-//   profileButton.style.cursor = "pointer";
-// });
-
-// profileButton.addEventListener('mouseleave', () => {
-//   profileButton.style.transform = 'scale(1)';
-//   profileButton.style.textShadow = "none";
-// });
-
-// profileButton.addEventListener('click', () => {
-//   profile.style.display = "flex";
-//   setTimeout(() => {
-//     profile.style.transform = "translateX(0)";
-//   }, 10);
-// });
-
-// if (screen.width > 1280) {
-//   profile.addEventListener('mouseleave', () => {
-//     profile.style.transform = "translateX(100%)";
-//     setTimeout(() => {
-//       profile.style.display = "none";
-//     }, 500);
-//   });
-// } else {
-//   closeProfile.addEventListener('click', () => {
-//     profile.style.transform = "translateX(100%)";
-//     setTimeout(() => {
-//       profile.style.display = "none";
-//     }, 500);
-//   });
-// }
-
-// profileLinks.forEach((link) => {
-//   link.addEventListener('mouseover', () => {
-//     link.style.color = "black";
-//     link.style.transform = "translateX(1rem)";
-//   });
-
-//   link.addEventListener('mouseout', () => {
-//     link.style.color = "gray";
-//     link.style.transform = "translateX(0)";
-//   });
-// });
-
 // MODAL FORMULARIO NUEVO LIBRO
-const newBookButton = document.querySelector('.newBookButton');
+const newBookButton = document.querySelectorAll('.newBookButton');
 const closeButton = document.querySelector('.closeButton');
 
-newBookButton.addEventListener('click', () => {
-  window.scrollTo(0,0);
-  document.querySelector('body').style.overflowY = "hidden";
-  document.querySelector('.new-book').style.display = "flex";
-  setTimeout(() => {
-    document.getElementById('newBookForm').style.transform = "scale(1)";
-  }, 10);
+newBookButton.forEach((button) => {
+  button.addEventListener('click', () => {
+    window.scrollTo(0,0);
+    document.querySelector('body').style.overflowY = "hidden";
+    document.querySelector('.new-book').style.display = "flex";
+    setTimeout(() => {
+      document.getElementById('newBookForm').style.transform = "scale(1)";
+    }, 10);
+  });
 });
 
 closeButton.addEventListener('click', () => {
@@ -254,4 +204,56 @@ alternativeButton.addEventListener('click', () => {
       alternative.classList.add('hidden');
     }, 500);
   }
+});
+
+// PERFIL
+const profileButton = document.querySelector('#profile');
+const profile = document.querySelector('.profile');
+const profileLinks = document.querySelectorAll('.profile a, .profile span');
+const closeProfile = document.querySelector('.close-profile');
+
+profileButton.addEventListener('mouseover', () => {
+  profileButton.style.transform = 'scale(1.1)';
+  profileButton.style.textShadow = ".3rem .3rem .3rem #00000050";
+  profileButton.style.cursor = "pointer";
+});
+
+profileButton.addEventListener('mouseleave', () => {
+  profileButton.style.transform = 'scale(1)';
+  profileButton.style.textShadow = "none";
+});
+
+profileButton.addEventListener('click', () => {
+  profile.style.display = "flex";
+  setTimeout(() => {
+    profile.style.transform = "translateX(0)";
+  }, 10);
+});
+
+if (screen.width > 1280) {
+  profile.addEventListener('mouseleave', () => {
+    profile.style.transform = "translateX(100%)";
+    setTimeout(() => {
+      profile.style.display = "none";
+    }, 500);
+  });
+} else {
+  closeProfile.addEventListener('click', () => {
+    profile.style.transform = "translateX(100%)";
+    setTimeout(() => {
+      profile.style.display = "none";
+    }, 500);
+  });
+}
+
+profileLinks.forEach((link) => {
+  link.addEventListener('mouseover', () => {
+    link.style.color = "black";
+    link.style.transform = "translateX(1rem)";
+  });
+
+  link.addEventListener('mouseout', () => {
+    link.style.color = "gray";
+    link.style.transform = "translateX(0)";
+  });
 });
--- views/main_menu.php
+++ views/main_menu.php
@@ -131,35 +131,49 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['form-action'])) {
         </form>
         </div>
         <!-- BOTON DEL MENU HAMBURGUESA -->
-        <!-- <button id="profile">
-          <i class="fa-solid fa-user"></i>
-        </button> -->
-        <br><br><br>
-        
-        <!-- <nav class="profile">
-          
-          <button class="close-profile">Itxi <i class="fa-solid fa-angles-right"></i></button>
-        </nav> -->
+        <div class="burguer-menu hidden">
+          <button id="profile">
+            <i class="fa-solid fa-bars"></i>
+          </button>
+          <aside class="profile">
+            <?php
+            echo '<div class="profile-img">
+                    <figure style="background: url(../src/img/profile/'.$_SESSION['profile_img'].'); background-position: center; background-size: cover;"></figure>
+                  </div>';
+            echo '<a href="main_menu.php"><i class="fa-solid fa-house"></i>Hasiera</a>
+                  <span class="newBookButton"><i class="fa-solid fa-book"></i>Igo liburu bat</span>
+                  <a href="personal_area.php"><i class="fa-solid fa-user"></i>Area pertsonala</a>';
+            if ($_SESSION['role'] === 'irakasle') {
+              echo '<a href="groups.php"><i class="fa-solid fa-users-rectangle"></i>Nire taldeak</a>
+                    <a href="requests.php"><i class="fa-solid fa-question"></i>Eskaerak</a>';
+            } else if ($_SESSION['role'] === 'admin') {
+              echo '<a href="management.php"><i class="fa-solid fa-gear"></i>Administrazioa</a></h1>';
+            }
+            echo '<a href="../modules/logout.php"><i class="fa-solid fa-user-slash"></i>Saioa itxi</a>';
+            ?>
+            <button class="close-profile">Itxi <i class="fa-solid fa-angles-right"></i></button>
+          </aside>
+        </div>
+        <div class="profile-pic">
+          <?php
+            echo '<a href="personal_area.php" style="background: url(../src/img/profile/'.$_SESSION['profile_img'].'); background-position: center; background-size: cover;"></a>';
+          ?>
+        </div>
       </section>
       <nav class="menu">
-
       <?php
-          // echo '<div class="profile-img">
-          //         <figure style="background: url(../src/img/profile/'.$_SESSION['profile_img'].'); background-position: center; background-size: cover;"></figure>
-          //       </div>';
-          echo '<a href="main_menu.php"><i class="fa-solid fa-house"></i>Hasiera</a>
-                <span class="newBookButton"><i class="fa-solid fa-book"></i>Igo liburu bat</span>
-                <a href="personal_area.php"><i class="fa-solid fa-user"></i>Area pertsonala</a>';
-          if ($_SESSION['role'] === 'irakasle') {
-            echo '<a href="groups.php"><i class="fa-solid fa-users-rectangle"></i>Nire taldeak</a>
-                  <a href="requests.php"><i class="fa-solid fa-question"></i>Eskaerak</a>';
-          } else if ($_SESSION['role'] === 'admin') {
-            echo '<a href="management.php"><i class="fa-solid fa-gear"></i>Administrazioa</a></h1>';
-          }
-          echo '<a href="../modules/logout.php"><i class="fa-solid fa-user-slash"></i>Saioa itxi</a>';
-          ?>
-          
-        </nav>
+        echo '<a href="main_menu.php"><i class="fa-solid fa-house"></i>Hasiera</a>
+              <span class="newBookButton"><i class="fa-solid fa-book"></i>Igo liburu bat</span>
+              <a href="personal_area.php"><i class="fa-solid fa-user"></i>Area pertsonala</a>';
+        if ($_SESSION['role'] === 'irakasle') {
+          echo '<a href="groups.php"><i class="fa-solid fa-users-rectangle"></i>Nire taldeak</a>
+                <a href="requests.php"><i class="fa-solid fa-question"></i>Eskaerak</a>';
+        } else if ($_SESSION['role'] === 'admin') {
+          echo '<a href="management.php"><i class="fa-solid fa-gear"></i>Administrazioa</a></h1>';
+        }
+        echo '<a href="../modules/logout.php"><i class="fa-solid fa-user-slash"></i>Saioa itxi</a>';
+      ?>
+      </nav>
     </header>
     <main>
       <?php
