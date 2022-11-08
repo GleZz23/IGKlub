9dbf8dad31dd49ba19092236802056c40d2dc6e2
Andriucm <andreimihalatxe@gmail.com>
2022-11-08 08:33:36 +0100

    -

--- views/main_menu.php
+++ views/main_menu.php
@@ -131,15 +131,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['form-action'])) {
         </form>
         </div>
         <!-- BOTON DEL MENU HAMBURGUESA -->
-        <!-- <button id="profile">
+        <button id="profile">
           <i class="fa-solid fa-user"></i>
-        </button> -->
-        <br><br><br>
-        
-        <!-- <nav class="profile">
+        </button>
+        <aside class="profile">
+          <?php
+          echo '<div class="profile-img">
+                  <figure style="background: url(../src/img/profile/'.$_SESSION['profile_img'].'); background-position: center; background-size: cover;"></figure>
+                </div>';
+          echo '<a href="personal_area.php"><i class="fa-solid fa-user"></i>Area pertsonala</a>';
+          echo '<a href="../modules/logout.php"><i class="fa-solid fa-user-slash"></i>Saioa itxi</a>';
           
+          ?>
           <button class="close-profile">Itxi <i class="fa-solid fa-angles-right"></i></button>
-        </nav> -->
+        </aside>
       </section>
       <nav class="menu">
 
@@ -148,19 +153,19 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['form-action'])) {
           //         <figure style="background: url(../src/img/profile/'.$_SESSION['profile_img'].'); background-position: center; background-size: cover;"></figure>
           //       </div>';
           echo '<a href="main_menu.php"><i class="fa-solid fa-house"></i>Hasiera</a>
-                <span class="newBookButton"><i class="fa-solid fa-book"></i>Igo liburu bat</span>
-                <a href="personal_area.php"><i class="fa-solid fa-user"></i>Area pertsonala</a>';
+                <span class="newBookButton"><i class="fa-solid fa-book"></i>Igo liburu bat</span>';
           if ($_SESSION['role'] === 'irakasle') {
             echo '<a href="groups.php"><i class="fa-solid fa-users-rectangle"></i>Nire taldeak</a>
                   <a href="requests.php"><i class="fa-solid fa-question"></i>Eskaerak</a>';
           } else if ($_SESSION['role'] === 'admin') {
             echo '<a href="management.php"><i class="fa-solid fa-gear"></i>Administrazioa</a></h1>';
           }
-          echo '<a href="../modules/logout.php"><i class="fa-solid fa-user-slash"></i>Saioa itxi</a>';
+          
           ?>
           
         </nav>
     </header>
+    
     <main>
       <?php
       // Recojo todos los valores de los libros en una variable
