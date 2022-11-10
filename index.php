7a2c0b4f0a17b9bccb711931d4120eb3e7f35198
GleZz23 <ikerglez110@gmail.com>
2022-11-10 19:33:57 +0100

    Responsive terminado

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
 <?php 
   include('templates/head.php');
 ?>
-
   <title>IGKlub</title>
   <link rel="stylesheet" href="styles/index.css">
   <link rel="stylesheet" href="styles/general.css">
 </head>
 <body>
-  <section>
-    <h1>IGKlub</h1> <!-- Cambiar por logo -->
-    <form action="views/signup.php" method="get">
-      <select name="role">
-        <option value="Irakasle">Irakasle</option>
-        <option value="Ikasle">Ikasle</option>
-      </select>
-      <p>bezala</p>
-      <button>erregistratu</button>
-    </form>
-    <div class="horizontal-bar"></div>
-    <a href="views/login.php">Saioa hasi</a>
-  </section>
+  <div>
+    <section>
+      <h1>IGKlub</h1> <!-- Cambiar por logo -->
+      <form action="views/signup.php" method="get">
+        <select name="role">
+          <option value="Irakasle">Irakasle</option>
+          <option value="Ikasle">Ikasle</option>
+        </select>
+        <p>bezala</p>
+        <button>erregistratu</button>
+      </form>
+      <div class="horizontal-bar"></div>
+      <a href="views/login.php">Saioa hasi</a>
+    </section>
+  </div>
 </body>
 </html>
